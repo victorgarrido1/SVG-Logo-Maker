b51eeac31a7587fe56aa310640bbadda6db0f287
victorgarrido1 <vcgarr94@gmail.com>
2024-01-11 16:43:04 -0700

    structing node.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//inquirer import statement 
+const inquirer = require("inquirer");
+
+//file system module import statement 
+const fs = require("fs");
+
+//import classes from the shapes directory
+const { Triangle, Circle, Square } = require()
