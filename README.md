4f9a80f9207311d544319b9ce2c0ee419b5a5ed2
victorgarrido1 <vcgarr94@gmail.com>
2024-01-15 22:56:02 -0700

    adding video link to read me and adding a emoji

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A Node.js command-line application that takes in user input to generate a logo a
 It utilizes the command-line to prompt a user a series of easy to follow simple questions. Once the user answers all the questions, then a SVG file is written using the selections based on the users answers and will generate a logo for them to appear. This project helped me learn the ability of the features that can be done on the back-end side without needing to have a user interface.Futhermore, I do hope to expand my learning and understand the many functionalities that can be performed.
 
 
+Here is a link that shows a video of the program in action: (https://watch.screencastify.com/v/aw9bbzZ6nhJ5dydwhbso)
+
+Thank you for viewing :white_check_mark:;
--- index.js
+++ index.js
@@ -1,101 +1,92 @@
-//inquirer import statement 
+//inquirer import statement
 const inquirer = require("inquirer");
 
-//file system module import statement 
+//file system module import statement
 const fs = require("fs");
-const { writeFile } = require("fs/promises")
+const { writeFile } = require("fs/promises");
 
 //import classes from the shapes directory
 const { Triangle, Circle, Square } = require("./lib/shapes");
 //imputing the text class
 const { Text } = require("./lib/text");
 
- {
+{
+  //files as a empty string
+  let svgString = "";
 
-    //files as a empty string
-let svgString = "";
-
-//sets the width and height of the logo container
-svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
-//continue here victor
-//tag g wraps into the text
-svgString += "<g>";
-
-
+  //sets the width and height of the logo container
+  svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+  //continue here victor
+  //tag g wraps into the text
+  svgString += "<g>";
 }
 
 //function that uses inquirer prompt to the user to answer questions
 function promptUser() {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type:"input",
-            message: "What would you like your logo to display?",
-            name: "text",
-        },
-        {
-            type:"input",
-            message: "Choose the text color (Enter a keyword)",
-            name: "textColor",
-        },
-        {
-            type:"list",
-            message: "What shape would you like the logo to render?",
-            choices: ["Triangle","Square", "Circle"],
-            name: "shape",
-        },
-        {
-            type:"input",
-            message: "What colors would you like your logo to have?",
-            name: "colors",
-        },
+      {
+        type: "input",
+        message: "What would you like your logo to display?",
+        name: "text",
+      },
+      {
+        type: "input",
+        message: "Choose the text color (Enter a keyword)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message: "What colors would you like your logo to have?",
+        name: "colors",
+      },
     ])
 
-    .then(( { text, textColor, shape, colors }) => {
-        let selectedShape;
+    .then(({ text, textColor, shape, colors }) => {
+      let selectedShape;
 
-        switch (shape) {
-            case "Triangle":
-                selectedShape = new Triangle();
-                break;
+      switch (shape) {
+        case "Triangle":
+          selectedShape = new Triangle();
+          break;
 
-                case "Square":
-                    selectedShape = new Square();
-                    break;
+        case "Square":
+          selectedShape = new Square();
+          break;
 
-            case "Circle":
-                selectedShape = new Circle();
-                break;
+        case "Circle":
+          selectedShape = new Circle();
+          break;
 
-                default: 
-                console.log("Invalid shape selected");
-                return;
-        }
+        default:
+          console.log("Invalid shape selected");
+          return;
+      }
 
-       //this is what renders the pictures or svg 
-        selectedShape.setColor(colors);
-        const textCol = new Text ();
-        textCol.setText(text, textColor);
-        const svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      //this is what renders the pictures or svg
+      selectedShape.setColor(colors);
+      const textCol = new Text();
+      textCol.setText(text, textColor);
+      const svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
                         <g>
                             ${selectedShape.render()}
                             ${textCol.render()}
                             
                         </g>
-                    </svg>`
-        
-                    ;
-        return writeFile("logo.svg", svg);
+                    </svg>`;
+
+      return writeFile("logo.svg", svg);
     })
     .catch((error) => {
-        console.error(error);
+      console.error(error);
     });
 }
 
-
-
 // Call the promptUser function to start the process
 promptUser();
-    
-
-
